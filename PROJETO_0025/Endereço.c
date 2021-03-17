84effd0577fafc6ef599ae35ffdb4263375db5bb
Victor <80796847+Victorissaimsm@users.noreply.github.com>
2021-03-17 01:17:31 -0300

    Add files via upload
    Sem alterações

---
+++ PROJETO_0025/Endereço.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+
+
+
+
+    struct Endereco
+    {
+
+        char Cidade[200];
+        char Bairro[200];
+        char Rua[200];
+        int num;
+        char complemento[20];
+    }End[5];
+
+   
+int main ()
+{
+
+    int i;
+
+      for (i =0; i < 5; i++)
+      {
+        printf ("\nInsira o nome da : \n", i+1);
+          fflush(stdin);
+          gets (End[i].Cidade);
+
+            printf ("\nInsira o endereco do %i professor: \n", i+1);
+              fflush (stdin);
+              gets (End[i].Bairro);
+
+        
+     
+     
+      } 
+
+       for (i =0; i < 5; i++)
+       {
+         printf ("%s ", End[i].Cidade);
+
+       }
+      return 0;
+}
