982afb1367f6df723f90a8166521de1eac4cbc87
Igor Gabriel Rodrigues <78672215+Igor0155@users.noreply.github.com>
2021-03-17 01:02:00 -0300

    Add files via upload

---
+++ PROJETO_0025/Inicio.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct professor
+{
+    char nome[70],disciplina[5],endereco[100];
+      int cpf, telefone, sala, matricula;
+
+} prof[5];
+
+
+
+int main ()
+{
+
+    int i;
+
+      for (i =0; i < 5; i++)
+      {
+        printf ("\nInsira o nome do %i professor: \n", i+1);
+          fflush(stdin);
+          gets (prof[i].nome);
+
+            printf ("\nInsira o endereco do %i professor: \n", i+1);
+              fflush (stdin);
+              gets (prof[i].endereco);
+
+      }
+
+       for (i =0; i < 5; i++)
+       {
+         printf ("%s ", prof[i].nome);
+
+       }
+      return 0;
+}
