6de29bf453b1685902d7417be439054ef94b339e
Caio ntbk <pmscaio@gmail.com>
2021-03-17 02:49:34 -0300

    teste de junção de estruturas

---
+++ PROJETO_0025/main/Endereço.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Endereco
+{
+    char Cidade[200];
+    char Bairro[200];
+    char Rua[200];
+    int num;
+    char complemento[20];
+}
---
+++ PROJETO_0025/main/teste prof ae.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct professor
+{
+    char nome[70];
+    int cpf, tel, sal, id;
+}
+
