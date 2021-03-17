cac492eb98c6449fbd2d9c705cc03a6af2e11f9e
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2021-03-17 15:00:56 -0300

    Update turma.c
    Adicionando informações adicionais a estrutura turma

--- PROJETO_0025/turma.c
+++ PROJETO_0025/turma.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// plano de aula
+// nomes alunos  && prof
+// Unidade curricular
+
 //Definindo a estrutura
-struct turma{
-char aluno[50];
-int turma, freq;
-float mediafreq, media_turma, nota;
-};
+struct turma_struct
+{
+    char aluno[50];
+    int turma, freq;
+    float mediafreq, media_turma, nota;
+}TURMA[5];
+
+void main()
+{
+    // Vazio por enquanto
+}
+
 
  
 
