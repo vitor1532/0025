fe9ee979468dcfef5a653ecede64d690714c446b
nikao8 <nicolasmarquesjf@hotmail.com>
2021-03-18 16:23:55 -0300

    Teste de txt

---
+++ PROJETO_0025/testeFile.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+    int x = 1, cont=0, y = 1;
+    FILE *file;
+
+    file = fopen("endLog.txt", "w");
+
+    char name[45], city[45], street[45];
+
+    do
+    {
+        cont++;
+        printf ("\n-------------------------------------\n           CADASTRO %i\n-------------------------------------\n", cont);
+        
+        setbuf(stdin,NULL);
+        printf ("\nEscreva seu nome: ");
+        fgets(name,45,stdin);
+        setbuf(stdin,NULL);
+
+        printf ("\nInsira sua cidade: ");
+        fgets(city,45,stdin);
+        setbuf(stdin,NULL);
+
+        printf ("\nInsira sua rua: ");
+        fgets(street,45,stdin);
+        setbuf(stdin,NULL);
+       
+        printf ("\nDeseja continuar os cadastros ? [1]SIM [2]NAO:\n");
+        scanf ("%i", &y); 
+
+        fprintf(file,"-------------------------------------\n             CADASTRO %i\n-------------------------------------\n", cont);
+        fprintf (file,"Nome: %s", name);
+        fprintf (file,"Cidade: %s", city);
+        fprintf (file,"Rua: %s", street);
+        fputs ("\n", file);
+          
+        if (y == 2)
+        {
+            fclose(file);
+        }
+        }while(y != 2);
+    
+}
+    
+
