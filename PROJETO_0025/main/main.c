ad7c855617c325f4b5aa063fc45e3c14d8df9f2a
CaioPereira <pmscaio@gmail.com>
2021-03-19 16:27:22 -0300

    main em construção

--- PROJETO_0025/main/main.c
+++ PROJETO_0025/main/main.c
@@ -1,1 +1,10 @@
 // arquivo principal, beberá de todas estruturas e funções
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "estruturas.c"
+#include "input_Usuarios copy.c"
+
+void main()
+{
+}
