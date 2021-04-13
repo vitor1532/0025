a2feebc76f0fe304ffb36ac35fcffa5644c74df4
Igor G. Rodrigues <igorgabriel0155@gmail.com>
2021-04-12 23:55:30 -0300

    Upload de exemplos de operadores em Python

---
+++ ALUNOS/Igor/Python/Operadores.py
@@ -0,0 +1,92 @@
+
+#Atribuindo Valores as variaveis
+var1 = 18
+var2 = 6
+
+
+print('\nVariaveis: Var1 = ',var1,', Var2 = ',var2)
+#Operador Soma '+'
+
+print('\n', var1, '+', var2,' =', var1 + var2 ) 
+
+#Operador Subtracao '-'
+
+print('\n', var1, '-', var2,' =', var1 - var2 ) 
+
+#Operdor Divisao Inteira '/' ___________________________
+
+print('\n', var1, '/', var2,' =', var1/var2)
+
+#Operdor Divisao Inteira '//' ___________________________
+
+print('\n', var1, '//', var2,' =', var1//var2)
+
+
+#Operdor Exponenciação '**'(Retorna um número elevado a potência de outro)  ___________________________
+
+print('\n', var1, '**', var2,' =', var1**var2)
+
+
+#Operdor Modulo '%' (Retorna o resto da divisão entre operandos) ___________________________
+
+print('\n', var1, '%', var2,' =', var1%var2)
+
+
+#Operador Maior que  '>'
+
+print('\nOperador Maior que ">"___________________')
+if var1 > var2:
+    print('variavel 1 =',var1)
+
+else:
+    print('variavel 2 =',var2)
+
+#Operdor Menor que '< '
+
+print('\nOperador Mair que "<"___________________')
+if var1 < var2:
+    print('variavel 1 =',var1)
+
+else:
+    print('variavel 2 =',var2)
+
+
+
+#Operador Maior que ou igual a  '>='
+
+print('\nOperador Mair que ou igual a ">="___________________')
+if var1 >= var2:
+    print(var1)
+else:
+    print(var2)
+
+
+
+#Operador Menor que ou igual a <=
+
+print('\nOperador Mair que ou igual a "<="___________________')
+if var1 <= var2:
+    print(var1)
+
+
+
+#Operdor Igual a '=='
+
+print('\nOperador Mair que "=="___________________')
+if var1 == var2:
+    print('variavel 1 =', var1)
+
+else:
+    print('variavel 2 =',var2)
+
+
+
+#Operdor Diferente de '!= '
+
+print('\nOperador Mair que "!="___________________')
+if var1 != var2:
+    print('variavel 1 =\n\n',var1)
+
+else:
+    print('variavel 2 =\n\n',var2)
+
